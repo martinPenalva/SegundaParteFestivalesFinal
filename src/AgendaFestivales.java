a34b456e584aead9e6e2d4d318be1cf0ab873e8a
mpenalvart1@educacion.navarra.es <mpenalvart1@educacion.navarra.es>
2024-03-04 15:47:39 +0100

    haciendo el addFestival

--- src/AgendaFestivales.java
+++ src/AgendaFestivales.java
@@ -1,1 +1,54 @@
 
+import java.util.*;
+
+
+/**
+ * Esta clase guarda una agenda con los festivales programados
+ * en una serie de meses
+ *
+ * La agenda guardalos festivales en una colección map
+ * La clave del map es el mes (un enumerado festivales.modelo.Mes)
+ * Cada mes tiene asociados en una colección ArrayList
+ * los festivales  de ese mes
+ *
+ * Solo aparecen los meses que incluyen algún festival
+ *
+ * Las claves se recuperan en orden alfabéico
+ *
+ */
+public class AgendaFestivales {
+    private TreeMap<Mes, ArrayList<Festival>> agenda;
+
+    public AgendaFestivales() {
+        this.agenda = new TreeMap<>();
+    }
+
+    /**
+     * añade un nuevo festival a la agenda
+     *
+     * Si la clave (el mes en el que se celebra el festival)
+     * no existe en la agenda se creará una nueva entrada
+     * con dicha clave y la colección formada por ese único festival
+     *
+     * Si la clave (el mes) ya existe se añade el nuevo festival
+     * a la lista de festivales que ya existe ese ms
+     * insertándolo de forma que quede ordenado por nombre de festival.
+     * Para este segundo caso usa el método de ayuda
+     * obtenerPosicionDeInsercion()
+     *
+     */
+    public void addFestival(Festival festival) {
+        //TODO
+        ArrayList<Festival> fes = new ArrayList<>();
+        Mes m = festival.getMes();
+        if (!this.agenda.containsKey(festival.getMes()))
+        {
+            fes.add(festival);
+            agenda.put(m, fes);
+        }
+        else
+        {
+            agenda.get(m).add(obtenerPosicionDeInsercion(f, festival), festival);
+        }
+
+    }
